23591bf7f6d54c4ac83e347c9cee64c1e331c460
王巍 <wei.wang90@foxmail.com>
2019-06-15 07:56:37 +0800

    init multi mongodb project

---
+++ spring-boot-mongodb/spring-boot-multi-mongodb/src/main/java/io/github/towerking/springbootmultimongodb/SpringBootMultiMongodbApplication.java
@@ -0,0 +1,13 @@
+package io.github.towerking.springbootmultimongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiMongodbApplication.class, args);
+    }
+
+}
