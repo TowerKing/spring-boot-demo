4cb3e0b77a39b7af8ed9a60eb996119273abecf9
王巍 <wei.wang90@foxmail.com>
2019-06-20 08:10:29 +0800

    init redis repository project

---
+++ spring-boot-redis/spring-boot-redis-repository/src/main/java/io/github/towerking/springbootredisrepository/SpringBootRedisRepositoryApplication.java
@@ -0,0 +1,13 @@
+package io.github.towerking.springbootredisrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisRepositoryApplication.class, args);
+    }
+
+}
