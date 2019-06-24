0ab6cb8b55aa36c022fac32dad46ebc6aecb4ece
王巍 <wei.wang90@foxmail.com>
2019-06-24 23:07:11 +0800

    init multi datasource jdbc project

---
+++ spring-boot-sql/spring-boot-multi-jdbc/src/main/java/io/github/towerking/springbootmultjdbc/SpringBootMultiJdbcApplication.java
@@ -0,0 +1,19 @@
+package io.github.towerking.springbootmultjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        JdbcTemplateAutoConfiguration.class })
+
+public class SpringBootMultiJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiJdbcApplication.class, args);
+    }
+
+}
