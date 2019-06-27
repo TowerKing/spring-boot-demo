9cbeb00bed67724a6998b1e540a873869e9c476f
王巍 <wei.wang90@foxmail.com>
2019-06-27 08:27:07 +0800

    add EnableTransactionManagement

--- spring-boot-sql/spring-boot-declarative-transaction/src/main/java/io/github/towerking/springbootdeclarativetransaction/SpringBootDeclarativeTransactionApplication.java
+++ spring-boot-sql/spring-boot-declarative-transaction/src/main/java/io/github/towerking/springbootdeclarativetransaction/SpringBootDeclarativeTransactionApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @Slf4j
 public class SpringBootDeclarativeTransactionApplication implements ApplicationRunner {
 
