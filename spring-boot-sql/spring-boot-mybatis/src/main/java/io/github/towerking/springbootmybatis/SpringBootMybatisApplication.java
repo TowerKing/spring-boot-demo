ed5c2e2cd1831ddf8135155eccbd6317d823ce31
王巍 <wei.wang90@foxmail.com>
2019-06-30 08:30:03 +0800

    add mybatis generator with issue: could not find BaseResultMap

--- spring-boot-sql/spring-boot-mybatis/src/main/java/io/github/towerking/springbootmybatis/SpringBootMybatisApplication.java
+++ spring-boot-sql/spring-boot-mybatis/src/main/java/io/github/towerking/springbootmybatis/SpringBootMybatisApplication.java
@@ -1,14 +1,24 @@
 package io.github.towerking.springbootmybatis;
 
+import io.github.towerking.springbootmybatis.mapper.StudentMapper;
 import io.github.towerking.springbootmybatis.mapper.UserMapper;
+import io.github.towerking.springbootmybatis.model.Student;
+import io.github.towerking.springbootmybatis.model.StudentExample;
 import io.github.towerking.springbootmybatis.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @Slf4j
@@ -18,12 +28,21 @@ public class SpringBootMybatisApplication implements ApplicationRunner {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private StudentMapper studentMapper;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMybatisApplication.class, args);
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // userCrud();
+        // generateArtifacts();
+        studentCrud();
+    }
+
+    private void userCrud() {
         User user = User.builder().name("Hello").build();
         int count = userMapper.save(user);
         log.info("Save {} User {}", count, user);
@@ -33,5 +52,37 @@ public class SpringBootMybatisApplication implements ApplicationRunner {
         log.info("Save {} User {}", count, user);
 
         log.info("Find User {}", userMapper.findById(1));
+    }
+
+    // 用于 myBatis 生成
+    private void generateArtifacts() throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(
+                this.getClass().getResourceAsStream("/generatorConfig.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    private void studentCrud() {
+        Student hello = new Student()
+                .withStudentName("hello")
+                .withStudentNo("001");
+        studentMapper.insert(hello);
+
+        Student world = new Student()
+                .withStudentName("world")
+                .withStudentNo("002");
+        studentMapper.insert(world);
+
+        log.info("Student 1: {}", studentMapper.selectByPrimaryKey(1));
+
+        StudentExample studentExample = new StudentExample();
+        studentExample.createCriteria().andStudentNameEqualTo("world");
+        studentMapper.selectByExample(studentExample).forEach(s -> {
+            log.info("select by example world: {}", s);
+        });
+
     }
 }
--- spring-boot-sql/spring-boot-mybatis/src/main/resources/schema.sql
+++ spring-boot-sql/spring-boot-mybatis/src/main/resources/schema.sql
@@ -5,3 +5,10 @@ create table t_user (
   update_time timestamp,
   primary key (id)
 );
+
+create table t_student (
+  id int(11) not null auto_increment,
+  student_no varchar(10) not null,
+  student_name varchar(30) not null,
+  primary key (id)
+);
