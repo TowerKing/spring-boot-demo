46ff60529fb334e3bec6400156a404846bd0ddd0
王巍 <wei.wang90@foxmail.com>
2019-07-02 23:40:53 +0800

    finish mongodb example

--- spring-boot-reactor/spring-boot-mongodb-reactor/src/main/java/io/github/towerking/springbootmongodbreactor/SpringBootMongodbReactorApplication.java
+++ spring-boot-reactor/spring-boot-mongodb-reactor/src/main/java/io/github/towerking/springbootmongodbreactor/SpringBootMongodbReactorApplication.java
@@ -1,15 +1,24 @@
 package io.github.towerking.springbootmongodbreactor;
 
+import io.github.towerking.springbootmongodbreactor.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import reactor.core.scheduler.Schedulers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootMongodbReactorApplication implements ApplicationRunner {
 
     @Autowired
@@ -23,6 +32,58 @@ public class SpringBootMongodbReactorApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // attention multi threads execute orders
+        // startFromInsertion(() -> log.info("Runnable"));
 
+        startFromInsertion(() -> {
+            log.info("Runnable");
+            grow();
+        });
+
+        log.info("after starting... ...");
+
+        // grow();
+
+        countDownLatch.await();
     }
+
+    private void startFromInsertion(Runnable runnable) {
+        mongoTemplate.insertAll(mockUser())
+                .publishOn(Schedulers.elastic())
+                .doOnNext(user -> log.info("Next: {}", user))
+                .doOnComplete(runnable)     // attention
+                .doFinally(s -> {
+                    countDownLatch.countDown();
+                    log.info("Finally 1, {}", s);
+                })
+                .count()
+                .subscribe(user -> log.info("Insert {} records", user));
+    }
+
+    private void grow() {
+        mongoTemplate.updateMulti(Query.query(Criteria.where("age").is(18)),
+                    new Update().inc("age", 1)
+                .currentDate("updateTime"), User.class)
+                .doFinally(s -> {
+                    countDownLatch.countDown();
+                    log.info("Finally 2, {}", s);
+                })
+                .subscribe(r -> log.info("Result is {}", r));
+    }
+
+
+    private List<User> mockUser() {
+        List<User> users = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            User user = User.builder()
+                    .name("Hello a" + i)
+                    .age(18 + i)
+                    .build();
+            users.add(user);
+        }
+
+        return users;
+    }
+
 }
