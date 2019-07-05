2a3e987965436d0d9bca599c2e247eb38fc4a9a5
王巍 <wei.wang90@foxmail.com>
2019-07-05 08:06:54 +0800

    init complex mvc project

---
+++ spring-boot-mvc/spring-boot-complex-mvc/src/main/java/io/github/towerking/springbootcomplexmvc/SpringBootComplexMvcApplication.java
@@ -0,0 +1,13 @@
+package io.github.towerking.springbootcomplexmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootComplexMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootComplexMvcApplication.class, args);
+    }
+
+}
