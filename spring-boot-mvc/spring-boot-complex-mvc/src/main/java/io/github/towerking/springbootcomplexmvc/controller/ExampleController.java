6ed2fced78485abde39e4505a5059c516c0f4bf0
王巍 <wei.wang90@foxmail.com>
2019-07-06 16:42:43 +0800

    add file controller

--- spring-boot-mvc/spring-boot-complex-mvc/src/main/java/io/github/towerking/springbootcomplexmvc/controller/ExampleController.java
+++ spring-boot-mvc/spring-boot-complex-mvc/src/main/java/io/github/towerking/springbootcomplexmvc/controller/ExampleController.java
@@ -3,11 +3,21 @@ package io.github.towerking.springbootcomplexmvc.controller;
 
 import io.github.towerking.springbootcomplexmvc.model.Example;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("example")
@@ -17,8 +27,46 @@ public class ExampleController {
     @PostMapping(path = "/", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseBody
     @ResponseStatus(HttpStatus.CREATED)
-    public Example addExampleWithOUtBindingResult(@Valid Example example) {
-        System.out.println(example.getCreateTime());
+    public Example addExampleWithoutBindingResult(@Valid Example example) {
         return example;
+    }
+
+    @PostMapping(path = "/binding", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public Example addExampleWithBindingResult(@Valid Example example, BindingResult result) {
+        if (result.hasErrors()) {
+            log.error("Binding errors: {}", result);
+            return null;
+        }
+        return example;
+    }
+
+    @PostMapping(path = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<Example> batchAddExamples(@RequestParam("file") MultipartFile file) {
+        List<Example> examples = new ArrayList<>();
+        if (file.isEmpty())
+            return examples;
+
+        BufferedReader reader = null;
+        try {
+            reader =new BufferedReader(new InputStreamReader(file.getInputStream()));
+            String str;
+            while ((str = reader.readLine()) != null) {
+                String[] arr = StringUtils.split(str, ",");
+                if (arr != null && arr.length == 2) {
+                    examples.add(new Example(Integer.parseInt(arr[0]), arr[1], new Date()));
+                }
+            }
+        } catch (IOException e) {
+            log.error("exception", e);
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(reader);
+        }
+
+        return examples;
     }
 }
--- spring-boot-mvc/spring-boot-complex-mvc/src/main/java/io/github/towerking/springbootcomplexmvc/model/Example.java
+++ spring-boot-mvc/spring-boot-complex-mvc/src/main/java/io/github/towerking/springbootcomplexmvc/model/Example.java
@@ -1,8 +1,6 @@
 package io.github.towerking.springbootcomplexmvc.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -10,6 +8,8 @@ import java.util.Date;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Example {
 
