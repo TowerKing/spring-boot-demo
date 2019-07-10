ba284e4b2fe7c653788cd19ed7fb42e9993bdbb4
王巍 <wei.wang90@foxmail.com>
2019-07-10 08:20:28 +0800

    init custom indicator project

---
+++ spring-boot-advanced-example/spring-boot-custom-indicator/src/main/java/io/github/towerking/springbootcustomindicator/SpringBootCustomIndicatorApplication.java
@@ -0,0 +1,13 @@
+package io.github.towerking.springbootcustomindicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomIndicatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCustomIndicatorApplication.class, args);
+    }
+
+}
